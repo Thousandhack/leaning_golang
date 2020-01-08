6f976b2189fa6ea27d35c618d5840d90f0225ee3
Thousand <569937624@qq.com>
2020-01-08 21:36:38 +0800

    转义字符与注释的学习

---
+++ chapter_02/excape_char/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"  // 主要提供格式化，输出和输入函数
+
+func main(){
+	// 演示转义字符
+	fmt.Println("tom\tjack")
+	fmt.Println("tom\njack")
+	fmt.Println("C:\\gostu\\go")
+	fmt.Println("I say:\"hello jack\"")
+	fmt.Println("is ready hard \rjack")
+}
+
