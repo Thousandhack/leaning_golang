0a93d4936e982215012904e84c3e3102f3430aed
Thousand <569937624@qq.com>
2020-02-16 21:13:15 +0800

    新增++和--

--- chapter_04/demo_operator/main.go
+++ chapter_04/demo_operator/main.go
@@ -2,10 +2,20 @@ package main
 
 import "fmt"
 
-func main()  {
+func main() {
 	// 运算符举例
+	var a int = 21
 	fmt.Println(+1)
 	fmt.Println(-1)
-	fmt.Println(5+5)
+	fmt.Println(5 + 5)
+	fmt.Println(5 - 3)
+	fmt.Println(3 * 4)
+	fmt.Println(6 / 3)
+	fmt.Println(6 % 4)
 
+	a--
+	fmt.Printf(" a 的值为 %d\n", a)
+	a = 21 // 为了方便测试，a 这里重新赋值为 21
+	a++
+	fmt.Printf(" a 的值为 %d\n", a)
 }
