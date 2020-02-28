4bd20e237c5dff6c756a7ef61fbb0e5537f67aa6
Thousand <569937624@qq.com>
2020-02-28 23:33:55 +0800

    go练习题for循环学习结束

--- chapter_05/fordeom/demo_03/main.go
+++ chapter_05/fordeom/demo_03/main.go
@@ -12,6 +12,14 @@ func main(){
 			count ++
 		}
 	}
-	fmt.Println("个数为：",num)
+	fmt.Println("个数为：",count)
 	fmt.Println("总和为：",sum)
+
+	// 两数字之和为0 ，一个从0变到6   一个从 6 变到0
+	var n int = 6
+	for i := 0; i <= n ; i++ {
+		fmt.Printf("%v + %v = %v\n",i,n-i,n)
+	}
+
+
 }
