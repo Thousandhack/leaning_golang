234964ac3d21d28724ee244e465bc08fd7f27f99
Thousand <569937624@qq.com>
2020-03-29 22:25:37 +0800

    切片的注意事项

--- chapter_07/slice/demo_for/main.go
+++ chapter_07/slice/demo_for/main.go
@@ -5,13 +5,17 @@ import "fmt"
 func main() {
 	// 使用常规的for循环遍历切片
 	var arr [5]int = [...]int{10, 20, 30, 40, 50}
-	slice := arr[1:4] // 第二个元素到第4个元素
-	for i := 0; i < len(slice); i++ {
-		fmt.Printf("i=%v v=%v\n",i,slice[i])
+	myslice := arr[1:4] // 第二个元素到第4个元素
+	for i := 0; i < len(myslice); i++ {
+		fmt.Printf("i=%v v=%v\n",i,myslice[i])
 	}
 
 	// 使用for--range方式遍历切片
-	for i, v := range slice {
+	for i, v := range myslice {
 		fmt.Printf("i=%v v=%v\n", i, v)
 	}
+
+	// 切片还可以切片
+	slice2 := myslice[1:]
+	fmt.Println(slice2)
 }
