0f736063d9dccdc0b4cd545e6f72e7ae7956dd20
Thousand <569937624@qq.com>
2020-04-27 22:49:05 +0800

    二维数组的开始

--- chapter_08/binary_sort/main.go
+++ chapter_08/binary_sort/main.go
@@ -16,11 +16,31 @@ import (
 
 3.想一下，怎么样的情况下，就说明找不到分析出退出递归的条件
 */
-func BinarySort(arr *[6]int,leftIndex int,rightIndex int,findVal int) {
+func BinarySort(arr *[6]int, leftIndex int, rightIndex int, findVal int) {
+	// 判断leftIndex 是否大于rightIndex
+	if leftIndex > rightIndex {
+		fmt.Println("找不到")
+		return
+	}
+
+	// 中间的下标
+	mid := (leftIndex + rightIndex) / 2
+
+	if (*arr)[mid] > findVal {
+		// 说明要查找的数，应该在left leftIndex --- mid -1
+		BinarySort(arr, leftIndex, mid-1, findVal)
+	} else if (*arr)[mid] < findVal {
+		// 说明要查找的数，应该在left mid + 1 ---- rightIndex
+		BinarySort(arr, mid+1, rightIndex, findVal)
+	} else {
+		// 找到了
+		fmt.Printf("下标为%v", mid)
+	}
 
 }
 
-func main(){
-	// 
-	arr := [6]int{1,8,10,89,1000,1234}
+func main() {
+	//
+	arr := [6]int{1, 8, 10, 89, 1000, 1234}
+	BinarySort(&arr, 0, len(arr)-1, 1000)
 }
