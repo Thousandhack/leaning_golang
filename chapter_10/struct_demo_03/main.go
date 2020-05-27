ccf4d8d19e230fe1f766547404a2f4effa6a401e
Thousand <569937624@qq.com>
2020-05-27 23:49:09 +0800

    结构体里面的属性之间不会相互影响

--- chapter_10/struct_demo_03/main.go
+++ chapter_10/struct_demo_03/main.go
@@ -2,15 +2,15 @@ package main
 
 import "fmt"
 
-type Cat struct{
+type Cat struct {
 	Name string
-	Age int
+	Age  int
 }
 
-
-func main(){
+func main() {
 
 	//  结构体里面的属性之间不会相互影响
+	// 结构体是值类型，默认是值拷贝
 	var cat1 Cat
 	cat1.Name = "bai"
 	cat1.Age = 2
