525f86d2939a1e003490045b2166ccf495e2428a
Thousandhack <1391053697@qq.com>
2020-08-21 23:01:24 +0800

    遍历数组接口

--- chapter_11/poly_demo/main.go
+++ chapter_11/poly_demo/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 // 声明/定义一个接口
-type Use interface {
+type Usb interface {
 	// 生命两个没有实现的方法
 	Start()
 	Stop()
@@ -29,6 +29,15 @@ type Camera struct {
 	Name string
 }
 
+func (computer Computer) Working(usb Usb){
+	usb.Start()
+	usb.Stop()
+}
+
+type Computer struct{
+
+}
+
 func (c Camera) Start() {
 	fmt.Println("相机开始工作。。。")
 
@@ -44,9 +53,17 @@ func main(){
 	// 这里就体现出多态数组
 	// 利用了接口多态的特点，原来一个数组只可以放一种类型，现在可以实现多种类型
 	// 多态数组
-	var useArr [3]Use
+	var useArr [3]Usb
 	useArr[0] = Phone{"vivo"}
 	useArr[1] = Phone{"mi"}
 	useArr[2] = Camera{"hua"}
+	// 遍历数组
+	// Phone还有一个特有的方法call() ，请遍历Use数组，如果是Phone变量
+	// 除了调用Usb 接口声明的方法外，还需要调用Phone 特有方法 call => 类型断言
+	var computer Computer
+	for _,v := range useArr{
+		computer.Working(v)
+	}
+
 	fmt.Println(useArr)
 }
