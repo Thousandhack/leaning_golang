145109d88a39150211e2a5ef78c82c37ad82a3de
Thousandhack <1391053697@qq.com>
2020-11-03 22:59:42 +0800

    bool类型的温习

--- chapter_03/demo_bool/main.go
+++ chapter_03/demo_bool/main.go
@@ -10,5 +10,8 @@ func main(){
 	fmt.Println("b=",b)
 	fmt.Println("b的占用空间=",unsafe.Sizeof(b))
 	// 2.bool类型只能去true或者false
+	// 3. Go语言中不允许将整形强制转换为布尔类型
+	// 4.布尔类型值无法参与数值预算，也无法与其他类型进行转换 （与python的区别）
+
 	
 }
