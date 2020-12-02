e37d30427dbd1a2fa2eb5db14c795923c4207890
Thousandhack <1391053697@qq.com>
2020-12-02 23:54:09 +0800

    时间格式化

--- go_base/time_demo/main.go
+++ go_base/time_demo/main.go
@@ -50,6 +50,7 @@ func timeOperateDemo(){
 	fmt.Println(later)
 	fmt.Println(now.Add(time.Minute*10)) // 10分钟后
 	// 定时器  可以做一个轮询的东西
+	// 定时器的本质上是一个通道（channel）。
 	timer := time.Tick(time.Second)
 	for t := range timer{
 		fmt.Println(t)
@@ -57,11 +58,37 @@ func timeOperateDemo(){
 	//now.Sub()
 }
 
+func formatDemo() {
+	now := time.Now()
+	/*
+	时间类型有一个自带的方法Format进行格式化，
+	需要注意的是Go语言中格式化时间模板不是常见的
+	Y-m-d H:M:S而是使用Go的诞生时间2006年1月2号15点04分
+	（记忆口诀为2006 1 2 3 4）。也许这就是技术人员的浪漫吧。
+	*/
+	// 格式化的模板为Go的出生时间2006年1月2号15点04分 Mon Jan
+	// 24小时制
+	fmt.Println(now.Format("2006-01-02 15:04:05.000 Mon Jan"))
+	// 12小时制
+	fmt.Println(now.Format("2006-01-02 03:04:05.000 PM Mon Jan"))
+	fmt.Println(now.Format("2006/01/02 15:04"))
+	fmt.Println(now.Format("15:04 2006/01/02"))
+	fmt.Println(now.Format("2006/01/02"))
+	// 按照对应的格式解析字符串类型的时间
+	timeObj,err := time.Parse("2006-01-02","2020-08-03")
+	if err != nil{
+		fmt.Printf("parse time failed,err:%v\n",err)
+		return
+	}
+	fmt.Println(timeObj)
+	fmt.Println(timeObj.Unix())
+}
+
 func main() {
 	timeDemoOne()
 	timestampDemo()
 	timestampToTime(1606788187)
 	// 时间操作
-	timeOperateDemo()
-	//  https://www.liwenzhou.com/posts/Go/go_time/    78  6分钟
+	//timeOperateDemo()
+	formatDemo()
 }
