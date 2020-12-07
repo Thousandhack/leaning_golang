013a2d1990115423c394e9360e221b5c5ec86b34
Thousandhack <1391053697@qq.com>
2020-12-08 00:23:43 +0800

    日志不同等级实现打印和保存到文件并且可以切割文件的实现

--- go_test/12_log_test/main.go
+++ go_test/12_log_test/main.go
@@ -2,10 +2,6 @@ package main
 
 import (
 	"gotest01/go_test/mylogger_test"
-	//"fmt"
-	//"log"
-	//"os"
-	"time"
 )
 
 //  可以往不同的输出位置记录日志
@@ -21,6 +17,9 @@ import (
 // 日志要支持开关控制  开发环境什么都输出，但是上线之后只有输出INFO
 // 日志的完整日志记录要有时间，行号，文件名，日志级别，日志信息
 // 日志文件要切割
+	// 1.按文件大小切割
+		// 1.每次记录日志之前都判断一下当前写的这个文件的文件的大小
+
 //
 // 第三方日志库：
 //如logrus、zap等
@@ -37,18 +36,20 @@ func main() {
 	//	time.Sleep(time.Second*3)
 	//}
 
-	log := mylogger_test.NewLog("debug")  // 设置将日志写入文件的级别
+	//log := mylogger_test.NewLog("debug")  // 设置将日志写入文件的级别
+
+	log := mylogger_test.NewFileLogger("Info", "./", "hsz.log", 10*1024)
 	// 实现可以穿字符串，也可以传格式化变量值
 	for {
 		name := "hsz"
-		log.Debug("这是一条debug级别的日志name:%s",name)
-		log.Info("这是一条info级别的日志name:%s",name)
+		log.Debug("这是一条debug级别的日志name:%s", name)
+		log.Info("这是一条info级别的日志name:%s", name)
 		log.Trance("这是一条Trance级别的日志")
 		log.Warning("这是一条Warning级别的日志")
 
 		log.Error("这是一条error级别的日志")
 		log.Fatal("这是一条Fatal级别的日志")
-		time.Sleep(time.Second * 3)
+		time.Sleep(time.Second * 1)
 	}
 
 }
