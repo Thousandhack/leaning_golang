1556fb59f0a8b4f244e71a2e0dba8668415c338d
Thousandhack <1391053697@qq.com>
2023-02-15 23:16:03 +0800

    add hello

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module gotest01
 
 go 1.13
 
-require github.com/go-sql-driver/mysql v1.5.0 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/jmoiron/sqlx v1.2.0 // indirect
+)
---
+++ go_base/var_demo/main.go
@@ -0,0 +1,29 @@
+package main
+
+// 导入语句
+import "fmt"
+
+// 函数外只能防止标识符（变量/常量/函数/类型）的声明
+// fmt.Printf("哈哈哈")  但是
+
+// 程序的主入口函数
+func main() {
+	// 打印完指定的内容之后会在后面加一个换行符
+	fmt.Println("哈哈哈")
+
+	// 单独声明变量
+	// var name string
+	// var age int
+	// var isOk bool
+	var (
+		name string
+		age  int
+		isOk bool
+	)
+	name = "hsz"
+	age = 25
+	isOk = false
+	// go 语言中非全局变量声明了必须使用
+	fmt.Printf("%s,%d,%d", name, age, isOk)
+
+}
