ea331f19daba4decf4d6f18a84690da6794f7ab8
Thousandhack <1391053697@qq.com>
2024-02-27 16:35:11 +0800

    feat stu map

--- go_review/025map/main.go
+++ go_review/025map/main.go
@@ -11,12 +11,27 @@ func main() {
 	m1["zero"] = 88
 	fmt.Println(m1)
 	fmt.Println(m1["hsz"])
-
+	// 约定俗称用ok 接收返回的布尔值
 	value, ok := m1["haha"]
 	if !ok {
 		fmt.Println("查无此key")
 	} else {
 		fmt.Println(value)
 	}
-	// 27 8分钟
+	// map的遍历
+	for k, v := range m1 {
+		fmt.Println(k, v)
+	}
+	// 只遍历key
+	for k := range m1 {
+		fmt.Println(k)
+	}
+	// 只遍历v
+	for _, v := range m1 {
+		fmt.Println(v)
+	}
+	delete(m1, "hsz")
+	fmt.Println(m1)
+	// 删除没有的元素就没有反应
+	delete(m1, "two")
 }
