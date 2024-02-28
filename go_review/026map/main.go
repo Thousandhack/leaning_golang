1aa5b82d582b3d263c3bc1cfd3e5fcfd1610c927
Thousandhack <1391053697@qq.com>
2024-02-28 23:48:27 +0800

    feat stu map

---
+++ go_review/026map/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+	"time"
+)
+
+func main() {
+	// 纳秒
+	//初始化随机数种子
+	//rand.Seed(time.Now().UnixNano()) // 这种方式1.20开始弃用
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	var scoreMap = make(map[string]int, 200)
+
+	for i := 0; i < 100; i++ {
+		key := fmt.Sprintf("stu%02d", i) //生成stu开头的字符串
+		value := rand.Intn(100)          //生成0~99的随机整数
+		scoreMap[key] = value
+	}
+	// 取出map中的所有key存入切片keys
+	var keys = make([]string, 0, 200)
+	for key := range scoreMap {
+		keys = append(keys, key)
+	}
+	// 对切片进行排序
+	sort.Strings(keys)
+	// 按照排序后的key遍历map
+	for _, key := range keys {
+		fmt.Println(key, scoreMap[key])
+	}
+}
