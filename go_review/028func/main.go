5690c6875969b70bba168648b061610e6099fde3
Thousandhack <1391053697@qq.com>
2024-03-03 20:29:17 +0800

    feat stu func

---
+++ go_review/028func/main.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+// 有参数有返回值的函数
+func f1(a int, b int) int {
+	return a + b
+}
+
+// 没有参数没有返回值的函数
+func f2() {
+	fmt.Println("f2")
+}
+
+// 没有参数但有返回值的函数
+func f3() int {
+	return 3
+}
+
+// 返回值可以也可以不命名
+func f4(x, y int) (ret int) {
+	ret = x + y
+	return
+}
+
+// 返回多个值
+func f5() (int, int) {
+	return 1, 2
+}
+
+func f6(x, y int, m, n string, i, j bool) int {
+	return x + y
+}
+
+// 可变长参数
+func f7(x string, y ...int) {
+	fmt.Println(x)
+	fmt.Println(y)
+}
+
+func main() {
+	fmt.Println(f1(3, 4))
+	f2()
+	fmt.Println(f3())
+	fmt.Println(f4(1, 2))
+	fmt.Println(f5())
+	f6(2, 3, "a", "b", true, false)
+	f7("下雨了", 1, 2, 3, 4, 5, 6, 7, 8, 9)
+	f7("下雨了")
+}
